2df662e990f7fd9413e75214f849ebb08b94c91d
Halil Ibrahim Disli <halil.disli@outlook.com>
2023-03-24 22:41:25 +0300

    DeleteArticle başarıyla yapılıyor.

--- Maidan/Maidan/Controllers/MemberController.cs
+++ Maidan/Maidan/Controllers/MemberController.cs
@@ -18,7 +18,7 @@ namespace Maidan.Controllers
         private readonly UserManager<Author> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly SignInManager<Author> _signInManager;
-        public MemberController(MaidanDbContext context, UserManager<Author> userManager, RoleManager<IdentityRole> roleManager,SignInManager<Author> signInManager)
+        public MemberController(MaidanDbContext context, UserManager<Author> userManager, RoleManager<IdentityRole> roleManager, SignInManager<Author> signInManager)
         {
             _context = context;
             _userManager = userManager;
@@ -40,17 +40,17 @@ namespace Maidan.Controllers
         public async Task<IActionResult> GetArticle(int id)
         {
             var article = _context.Articles.Find(id);
-			var identityUser = await _userManager.FindByNameAsync(User.Identity.Name);
-			var top3ArticlesForUser = _context.Articles.Where(a => a.AuthorId == identityUser.Id).OrderByDescending(a=>a.ReleaseDate).Take(3).ToList();
+            var identityUser = await _userManager.FindByNameAsync(User.Identity.Name);
+            var top3ArticlesForUser = _context.Articles.Where(a => a.AuthorId == identityUser.Id).OrderByDescending(a => a.ReleaseDate).Take(3).ToList();
             ViewBag.Top3Articles = top3ArticlesForUser;
             ViewBag.Author = identityUser;
-			return View(article);
+            return View(article);
         }
 
         [HttpGet]
         public async Task<IActionResult> SignOut()
         {
-            return RedirectToAction("Index","Home");
+            return RedirectToAction("Index", "Home");
         }
         [HttpGet]
         public IActionResult CreateArticle()
@@ -77,91 +77,108 @@ namespace Maidan.Controllers
             }
             return View();
         }
-		[HttpGet]
-		public IActionResult UpdateArticle(int id)
-		{
-			Article foundedArticle=_context.Articles.Find(id);
-			ArticleViewModel articleViewModel = new ArticleViewModel() { Id=foundedArticle.Id,Title = foundedArticle.Title, Content = foundedArticle.Content, Image = foundedArticle.Image };
+        [HttpGet]
+        public IActionResult UpdateArticle(int id)
+        {
+            Article foundedArticle = _context.Articles.Find(id);
+            ArticleViewModel articleViewModel = new ArticleViewModel() { Id = foundedArticle.Id, Title = foundedArticle.Title, Content = foundedArticle.Content, Image = foundedArticle.Image };
 
-			return View(articleViewModel);
-		}
-		[HttpPost]
-		public IActionResult UpdateArticle(ArticleViewModel articleViewModel)
-		{
-			if (ModelState.IsValid)
-			{
-				Article toBeUpdated = _context.Articles.Find(articleViewModel.Id);
-				toBeUpdated.UpdateDate = DateTime.Now;
-				toBeUpdated.Title = articleViewModel.Title;
-				toBeUpdated.Content = articleViewModel.Content;
-				toBeUpdated.Image = articleViewModel.Image;
-				_context.Articles.Update(toBeUpdated);
-				_context.SaveChanges();
-				return RedirectToAction("Index", "Member");
-			}
-			return View();
-		}
+            return View(articleViewModel);
+        }
+        [HttpPost]
+        public IActionResult UpdateArticle(ArticleViewModel articleViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                Article toBeUpdated = _context.Articles.Find(articleViewModel.Id);
+                toBeUpdated.UpdateDate = DateTime.Now;
+                toBeUpdated.Title = articleViewModel.Title;
+                toBeUpdated.Content = articleViewModel.Content;
+                toBeUpdated.Image = articleViewModel.Image;
+                _context.Articles.Update(toBeUpdated);
+                _context.SaveChanges();
+                return RedirectToAction("Index", "Member");
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult DeleteArticle(int id)
+        {
+            Article foundedArticle = _context.Articles.Find(id);
+            ArticleViewModel articleViewModel = new ArticleViewModel() { Id = foundedArticle.Id, Title = foundedArticle.Title, Content = foundedArticle.Content, Image = foundedArticle.Image };
+
+            return View(articleViewModel);
+        }
+        [HttpPost]
+        public IActionResult DeleteArticle(ArticleViewModel articleViewModel)
+        {
+
+            Article toBeDeleted = _context.Articles.Find(articleViewModel.Id);
+            _context.Articles.Remove(toBeDeleted);
+            _context.SaveChanges();
+            return RedirectToAction("Index", "Member");
+        }
 
 
-		//[HttpGet]
-		//[Route("SignIn")]
-		//public IActionResult SignIn()
-		//{
-		//    ClaimsPrincipal claimUser = HttpContext.User;
-		//    if (claimUser.Identity.IsAuthenticated)
-		//    {
-		//        return RedirectToAction("Index", "Member");
-		//    }
-		//    return View();
-		//}
-		//[HttpPost]
-		//[Route("SignIn")]
-		//public async Task<IActionResult> SignIn(VMLogin loginModel)
-		//{
-		//    var foundedUser = _context.Users.Where(u => u.Email == loginModel.Email).FirstOrDefault();
-		//    var hashedPassword = HashPassword(loginModel.Password);
-		//    if (foundedUser != null && foundedUser.PasswordHash == hashedPassword)
-		//    {
-		//        List<Claim> claims = new List<Claim>()
-		//        {
-		//            new Claim(ClaimTypes.NameIdentifier,loginModel.Email),
-		//            new Claim("Other Properties","Example Role")
-		//        };
-		//        ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+        //[HttpGet]
+        //[Route("SignIn")]
+        //public IActionResult SignIn()
+        //{
+        //    ClaimsPrincipal claimUser = HttpContext.User;
+        //    if (claimUser.Identity.IsAuthenticated)
+        //    {
+        //        return RedirectToAction("Index", "Member");
+        //    }
+        //    return View();
+        //}
+        //[HttpPost]
+        //[Route("SignIn")]
+        //public async Task<IActionResult> SignIn(VMLogin loginModel)
+        //{
+        //    var foundedUser = _context.Users.Where(u => u.Email == loginModel.Email).FirstOrDefault();
+        //    var hashedPassword = HashPassword(loginModel.Password);
+        //    if (foundedUser != null && foundedUser.PasswordHash == hashedPassword)
+        //    {
+        //        List<Claim> claims = new List<Claim>()
+        //        {
+        //            new Claim(ClaimTypes.NameIdentifier,loginModel.Email),
+        //            new Claim("Other Properties","Example Role")
+        //        };
+        //        ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-		//        AuthenticationProperties properties = new AuthenticationProperties()
-		//        {
-		//            AllowRefresh = true,
-		//            IsPersistent = loginModel.RememberMe
+        //        AuthenticationProperties properties = new AuthenticationProperties()
+        //        {
+        //            AllowRefresh = true,
+        //            IsPersistent = loginModel.RememberMe
 
-		//        };
+        //        };
 
-		//        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), properties);
+        //        await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), properties);
 
-		//        TempData["ValidateMessage"] = "Login successfully!";
-		//        return RedirectToAction("Index", "Member");
-		//    }
+        //        TempData["ValidateMessage"] = "Login successfully!";
+        //        return RedirectToAction("Index", "Member");
+        //    }
 
-		//    TempData["ValidateMessage"] = "User not found!!";
-		//    return View();
-		//}
-		//public static string HashPassword(string password)
-		//{
-		//    byte[] salt;
-		//    byte[] buffer2;
-		//    if (password == null)
-		//    {
-		//        throw new ArgumentNullException("password");
-		//    }
-		//    using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8))
-		//    {
-		//        salt = bytes.Salt;
-		//        buffer2 = bytes.GetBytes(0x20);
-		//    }
-		//    byte[] dst = new byte[0x31];
-		//    Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
-		//    Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
-		//    return Convert.ToBase64String(dst);
-		//}
-	}
+        //    TempData["ValidateMessage"] = "User not found!!";
+        //    return View();
+        //}
+        //public static string HashPassword(string password)
+        //{
+        //    byte[] salt;
+        //    byte[] buffer2;
+        //    if (password == null)
+        //    {
+        //        throw new ArgumentNullException("password");
+        //    }
+        //    using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8))
+        //    {
+        //        salt = bytes.Salt;
+        //        buffer2 = bytes.GetBytes(0x20);
+        //    }
+        //    byte[] dst = new byte[0x31];
+        //    Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
+        //    Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
+        //    return Convert.ToBase64String(dst);
+        //}
+    }
 }
